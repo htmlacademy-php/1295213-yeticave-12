99576aeb806fb15db3c8c5855c4b4c5979285a3a
Eduard <Eduard.kgn@gmail.com>
2021-04-20 21:36:30 +0200

    module4-task1 создание базы данных и таблиц. Думаю нужны будут комментарии, paiza.io ошибок нет, но в phpmyadmin ошибка - #1064 - У вас ошибка в запросе. Изучите документацию по используемой версии MySQL на предмет корректного синтаксиса около 'CREATE TABLE category (     id     INT AUTO_INCREMENT PRIMARY KEY,     name_c' на строке 3

---
+++ schema.sql
@@ -0,0 +1,70 @@
+CREATE
+DATABASE yeticave
+DEFAULT CHARACTER SET utf8
+DEFAULT COLLATE utf8_general_ci;
+USE yeticave
+
+CREATE TABLE category
+(
+    id     INT AUTO_INCREMENT PRIMARY KEY,
+    name_category   CHAR NOT NULL,
+    s_code CHAR NOT NULL
+);
+
+CREATE TABLE lot
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    add_time    DATETIME NOT NULL,
+    name_lot        CHAR     NOT NULL UNIQUE,
+    description TEXT     NOT NULL,
+    img         CHAR     NOT NULL UNIQUE,
+    start_price INT      NOT NULL,
+    data_finish DATETIME NOT NULL,
+    step_rate   INT      NOT NULL,
+
+    author_id   INT      NOT NULL,
+    winner_id   INT      NOT NULL,
+    category_id INT      NOT NULL,
+    FOREIGN KEY (author_id) REFERENCES site_user (id),
+    FOREIGN KEY (winner_id) REFERENCES site_user (id),
+    FOREIGN KEY (category_id) REFERENCES category (id)
+);
+
+CREATE TABLE rate
+(
+
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    add_time DATETIME NOT NULL,
+    price    INT      NOT NULL,
+
+    user_id  INT      NOT NULL,
+    lot_id   INT      NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES site_user (id),
+    FOREIGN KEY (lot_id) REFERENCES lot (id)
+);
+
+CREATE TABLE site_user
+(
+    id       INT AUTO_INCREMENT PRIMARY KEY,
+    data_reg DATETIME NOT NULL,
+    email    CHAR     NOT NULL UNIQUE,
+    name_user     CHAR     NOT NULL,
+    password CHAR     NOT NULL,
+    contacts CHAR     NOT NULL,
+
+    lot_id   INT      NOT NULL,
+    rate_us  INT      NOT NULL,
+    FOREIGN KEY (lot_id) REFERENCES lot (id),
+    FOREIGN KEY (rate_us) REFERENCES rate (price)
+);
+
+CREATE
+UNIQUE INDEX name_category ON category(name_category);
+CREATE
+UNIQUE INDEX name_l ON lot(name_lot);
+CREATE
+UNIQUE INDEX name_user ON site_user(name_user);
+CREATE
+INDEX price ON rate(price);
+CREATE
+INDEX data_finish ON lot(data_finish);
